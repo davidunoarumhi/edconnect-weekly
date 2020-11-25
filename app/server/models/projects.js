bbc72bb6b97e4c9b54fa5b9c0352fc0de176287f
unknown <unodavid06@gmail.com>
2020-11-25 12:27:34 +0100

    Moved week-5 files from edconnect-weekly-2 repo

--- app/server/models/projects.js
+++ app/server/models/projects.js
@@ -2,13 +2,27 @@ const DataModel = require('./data_model');
 
 class Project {
     constructor(id, name, abstract, authors, tags, createdBy) {
-
+        this.id = id;
+        this.name = name;
+        this. abstract = abstract;
+        this.authors = authors;
+        this.tags = tags;
+        this.createdBy = createdBy;
     }
 }
 
 class Projects extends DataModel {
     validate(obj) {
-
+        if (!Array.isArray(obj.authors)) {
+            return false;
+        }
+        if (!Array.isArray(obj.tags)) {
+            return false;
+        }
+        if (obj.name.length === 0 || obj.abstract.length === 0 || obj.authors.length === 0 || obj.tags.length === 0 || obj.createdBy.length === 0) {
+            return false;
+        }
+        return true;
     }
 }
 
--- app/server/models/users.js
+++ app/server/models/users.js
@@ -2,29 +2,72 @@ const DataModel = require('./data_model');
 
 class User {
     constructor(id, firstname, lastname, email, password, matricNumber, program, graduationYear) {
-
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.matricNumber = matricNumber;
+        this.program = program;
+        this.graduationYear = graduationYear;
     }
 
     getFullName() {
-
+        return this.firstname + " " + this.lastname;
     }
 }
 
 class Users extends DataModel {
     authenticate(email, password) {
-
+        var user = this.getByEmail(email);
+        if (user != null) {
+            if (user.password === password) {
+                return true;
+            }
+            return false;
+        }
+        return false;
     }
 
     getByEmail(email) {
-
+        let obj = this.data.find(obj => obj.email == email);
+        if (obj === undefined) {
+            return null;
+        } else {
+            return obj;
+        }
     }
 
     getByMatricNumber(matricNumber) {
-
+        let obj = this.data.find(obj => obj.matricNumber == matricNumber);
+        if (obj === undefined) {
+            return null;
+        } else {
+            return obj;
+        }
     }
 
     validate(obj) {
-
+        if (obj.firstname.length == 0 ||
+             obj.lastname.length == 0 ||
+              obj.email.length == 0 ||
+               obj.password.length == 0 ||
+                obj.matricNumber.length == 0 ||
+                 obj.program.length == 0 ||
+                  obj.graduationYear.length == 0
+                  ) {
+            return false;
+        }
+        if ((this.data.find(test => test.email == obj.email)) !== undefined) {
+            return false;
+        }
+        if ((this.data.find(test => test.matricNumber == obj.matricNumber)) !== undefined) {
+            return false;
+        }
+        if (obj.password.length < 7) {
+            return false;
+        }
+        return true;
     }
 }
 
